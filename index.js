5963103b5b4cb5705b548f0aa86994daa79c3b50
julian <juliansanvill@hotmail.com>
2023-02-06 16:51:02 -0500

    agregando variables e interacciones con el menu desplegable

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-document.addEventListener("DOMContentLoaded",function(){
-    document.getElementById("menu-button").addEventListener("click",showMenu)
-    document.getElementById("close-menu").addEventListener("click",closeMenu)
-
-   function showMenu(){
-    const menu = document.querySelector("section.menu");
-    menu.style.height="100%"
-    menu.style.display="block"
-   }
-   function closeMenu(){
-    const menu=document.querySelector("section.menu");
-    menu.style.height="0";
-    menu.style.display="none"
-   }
+import hamburgerMenu from "./hamburguer.js";
+const d=document;
+d.addEventListener("DOMContentLoaded",e=>{
+    hamburgerMenu(".panel-btn",".panel");
 })
