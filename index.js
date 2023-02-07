fe3b5cdd624bf562a79445bac46a33b064c1351a
julian <juliansanvill@hotmail.com>
2023-02-07 18:34:12 -0500

    agregando el evento numero 2 evento de teclado

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import hamburgerMenu from "./hamburguer.js";
 import { digitalClock,Alarm } from "./reloj.js";
+import { moveBall,shortcuts } from "./teclado.js";
+
 const d=document;
+
 d.addEventListener("DOMContentLoaded",e=>{
     hamburgerMenu(".panel-btn",".panel",".menu a");
     digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
     Alarm("assets/alarma.mp3.mp3","#activar-alarma","#desactivar-alarma");
+})
+
+
+d.addEventListener("keydown",(e)=>{
+    shortcuts(e);
+    moveBall(e,".ball",".stage");
 })
