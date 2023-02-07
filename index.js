9a9081af9c527dbbf046704b00ff2797bd37349f
julian <juliansanvill@hotmail.com>
2023-02-07 15:58:51 -0500

    agregando eventos de reloj y de sonido en alarm

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import hamburgerMenu from "./hamburguer.js";
+import { digitalClock,Alarm } from "./reloj.js";
 const d=document;
 d.addEventListener("DOMContentLoaded",e=>{
-    hamburgerMenu(".panel-btn",".panel");
+    hamburgerMenu(".panel-btn",".panel",".menu a");
+    digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
+    Alarm("assets/alarma.mp3.mp3","#activar-alarma","#desactivar-alarma");
 })
