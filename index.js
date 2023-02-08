29aacbb80e87941aff2309ad2d1dad0e3ec198e8
julian <juliansanvill@hotmail.com>
2023-02-08 15:34:25 -0500

    agregando contador en ejercicio 3

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+
+
+
 import hamburgerMenu from "./hamburguer.js";
 import { digitalClock,Alarm } from "./reloj.js";
 import { moveBall,shortcuts } from "./teclado.js";
+import countdown from "./cuenta_regresiva.js";
 
 const d=document;
 
@@ -8,6 +12,7 @@ d.addEventListener("DOMContentLoaded",e=>{
     hamburgerMenu(".panel-btn",".panel",".menu a");
     digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
     Alarm("assets/alarma.mp3.mp3","#activar-alarma","#desactivar-alarma");
+    countdown("countdown","mayo 11,2023 03:23:19","Feliz Cumpleaños");
 })
 
 
